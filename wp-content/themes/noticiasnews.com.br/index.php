26a5f2460f1e0e56652c014e7dfd7215d8e2edd8
= <=>
2023-07-18 08:01:45 -0300

    <CHORE> Finalizating HomePage

--- wp-content/themes/noticiasnews.com.br/index.php
+++ wp-content/themes/noticiasnews.com.br/index.php
@@ -2,55 +2,6 @@
 get_header();
 ?>
 
-<main class="min-h-screen">
-    <section class="flex w-1/2"></section>
-    <section class="flex w-1/2 flex-col">
-        <div class="flex gap-4">
-            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
-            <div class="flex flex-col justify-between max-w-xs gap-2">
-                <div class="categories">
-                    <div class="rounded bg-red text px-2 py-1 uppercase text-white max-w-max font-semibold">Saúde/Bem Estar</div>
-                </div>
-                <h2 class="text-lg font-medium text-black truncate">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
-                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
-            </div>
-        </div>
-        <div class="h-px bg-gray w-full my-4"></div>
-        <div class="flex gap-4">
-            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
-            <div class="flex flex-col justify-between max-w-xs gap-2">
-                <div class="categories">
-                    <div class="rounded bg-red text px-2 py-1 uppercase text-white max-w-max font-semibold">Saúde/Bem Estar</div>
-                </div>
-                <h2 class="text-lg font-medium text-black truncate">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
-                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
-            </div>
-        </div>
-        <div class="h-px bg-gray w-full my-4"></div>
-        <div class="flex gap-4">
-            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
-            <div class="flex flex-col justify-between max-w-xs gap-2">
-                <div class="categories">
-                    <div class="rounded bg-red text px-2 py-1 uppercase text-white max-w-max font-semibold">Saúde/Bem Estar</div>
-                </div>
-                <h2 class="text-lg font-medium text-black truncate">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
-                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
-            </div>
-        </div>
-        <div class="h-px bg-gray w-full my-4"></div>
-        <div class="flex gap-4">
-            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
-            <div class="flex flex-col justify-between max-w-xs gap-2">
-                <div class="categories">
-                    <div class="rounded bg-red text px-2 py-1 uppercase text-white max-w-max font-semibold">Saúde/Bem Estar</div>
-                </div>
-                <h2 class="text-lg font-medium text-black truncate">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
-                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
-            </div>
-        </div>
-    </section>
-</main>
-
 <?php
 get_footer();
 ?>
---
+++ wp-content/themes/noticiasnews.com.br/page-home.php
@@ -0,0 +1,87 @@
+/*
+* Template Name: Home
+*/
+<main class="flex bg-white py-20 px-32">
+    <!-- LEFT PART --> 
+    <section class="flex w-1/2 flex-col gap-4">
+        <div class="flex flex-col gap-4">
+            <div class="categories">
+                <div class="category">Saúde/Bem Estar</div>
+            </div>
+            <h2 class="title-notice text-xl">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-11/12 h-80">
+        </div>
+
+        <div class="space-horizontal w-11/12"></div>
+
+        <div class="flex gap-4">
+        <div class="flex gap-4">
+            <div class="card-notice">
+                <div class="categories">
+                    <div class="category">Saúde/Bem Estar</div>
+                </div>
+                <h2 class="title-notice text-lg">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
+            </div>
+        </div>
+        <div class="space-vertical h-11/12"></div>
+        <div class="flex gap-4">
+            <div class="card-notice">
+                <div class="categories">
+                    <div class="category">Saúde/Bem Estar</div>
+                </div>
+                <h2 class="title-notice text-lg">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
+            </div>
+        </div>
+        </div>
+        <button  class="btn justify-center p-2 w-11/12">Ver mais <i class="ph ph-user-plus"></i></button>
+    </section>
+
+    <!-- RIGTH PART --> 
+    <section class="flex w-1/2 flex-col">
+        <div class="flex gap-4">
+            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
+            <div class="card-notice">
+                <div class="categories">
+                    <div class="category">Saúde/Bem Estar</div>
+                </div>
+                <h2 class="title-notice text-lg">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
+            </div>
+        </div>
+        <div class="space-horizontal w-full"></div>
+        <div class="flex gap-4">
+            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
+            <div class="card-notice">
+                <div class="categories">
+                    <div class="category">Saúde/Bem Estar</div>
+                </div>
+                <h2 class="title-notice text-lg">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
+            </div>
+        </div>
+        <div class="space-horizontal w-full"></div>
+        <div class="flex gap-4">
+            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
+            <div class="card-notice">
+                <div class="categories">
+                    <div class="category">Saúde/Bem Estar</div>
+                </div>
+                <h2 class="title-notice text-lg">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
+            </div>
+        </div>
+        <div class="space-horizontal w-full"></div>
+        <div class="flex gap-4">
+            <img src="<?= get_template_directory_uri()?>/src/img/noticia-sem-imagem.jpg" alt="Imagem da Noticia" class="w-44 h-32">
+            <div class="card-notice">
+                <div class="categories">
+                    <div class="category">Saúde/Bem Estar</div>
+                </div>
+                <h2 class="title-notice text-lg">Os hábitos que contaminam a comida e matam pessoas todos os anos</h2>
+                <span class="text-gray">Por <span class="text-red">Ana Lise </span> - 1 de abr 2023 07h15</span>
+            </div>
+        </div>
+    </section>
+</main>
